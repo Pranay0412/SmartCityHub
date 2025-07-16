acc52c2bdab4ad98f2b04710520b2e445699fe7f
PRANAY JAIN <jainpranaytaral@gmail.com>
2025-07-16 20:31:48 +0530

    init commit

--- src/DataBase/RouteDAO.java
+++ src/DataBase/RouteDAO.java
@@ -3,4 +3,7 @@ package DataBase;
 import SmartCity.Route;
 
 public class RouteDAO {
+    public void addRoute(Route route){}
+    public void getRouteById(int routeId){}
+    public void getAllRoutes(){}
 }
--- src/SmartCity/Route.java
+++ src/SmartCity/Route.java
@@ -1,5 +1,25 @@
 package SmartCity;
 
 public class Route {
+    int id;
+    String name;
+    String type;
 
+    public Route(int id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
