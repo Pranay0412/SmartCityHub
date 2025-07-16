1d8822c7f4b302a6553f6abf4ff92a5417ec6cb8
PRANAY JAIN <jainpranaytaral@gmail.com>
2025-07-16 20:32:51 +0530

    init commit

--- src/DataBase/StationDAO.java
+++ src/DataBase/StationDAO.java
@@ -6,5 +6,5 @@ public class StationDAO {
     public void addStation(Station station){}
     public void getStationById(int stationID){}
     public void getAllStops(){}
-
+    public void getStopsByRouteId(int routeId){}
 }
