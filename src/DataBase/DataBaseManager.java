bf33d2b059ee9e94c400d771eef77d88f8eafb9d
PRANAY JAIN <jainpranaytaral@gmail.com>
2025-07-17 18:36:20 +0530

    init commit

--- src/DataBase/DataBaseManager.java
+++ src/DataBase/DataBaseManager.java
@@ -1,10 +1,31 @@
 package DataBase;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.SQLOutput;
 
 public class DataBaseManager {
-    public void getInstance(){}
-    public void getConnection(){}
-    public void closeConnectin(Connection con){}
+    Connection connection;
+    public void getInstance() throws ClassNotFoundException {
+        String driverManager = "com.mysql.cj.jdbc.Driver";
+        Class.forName(driverManager);
+        System.out.println("Driver Registered.");
+    }
+    public void getConnection() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/SmartCityHub";
+        String user = "root";
+        String password = "";
+
+        connection = DriverManager.getConnection(url, user, password);
+        if(connection != null) {
+            System.out.println("Connected");
+        } else {
+            System.out.println("Failed.");
+        }
+    }
+    public void closeConnectin(Connection con) throws SQLException {
+        con.close();
+    }
 
 }
