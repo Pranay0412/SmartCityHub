3f3380868aa0250243807a1ccd17b24383cdb86e
PRANAY JAIN <jainpranaytaral@gmail.com>
2025-07-23 21:50:09 +0530

    init commit

---
+++ src/DataBase/TicketDAO.java
@@ -0,0 +1,128 @@
+package DataBase;
+
+import Model.Ticket;
+
+import java.sql.*;
+import java.io.*;
+
+public class TicketDAO {
+    private Connection connection = DataBaseManager.connection;
+
+    public TicketDAO() {
+        try {
+            DataBaseManager.getConnection();
+            connection.setAutoCommit(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Add a new Ticket.
+     *
+     * @param ticket Object of Ticket
+     * @return true if new ticket is added
+     */
+    public boolean addTicket(Ticket ticket) {
+        String query = "INSERT INTO ticket (UserID, RouteId, IsBusTransport, IsMetroTransport, Time, TotalBill, Distance) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setInt(1, ticket.getUserId());
+            stmt.setInt(2, ticket.getRouteId());
+            stmt.setBoolean(3, ticket.isBusTransport());
+            stmt.setBoolean(4, ticket.isMetroTransport());
+            stmt.setTime(5, ticket.getTime());
+            stmt.setDouble(6, ticket.getTotalBill());
+            stmt.setDouble(7, ticket.getDistance());
+
+            System.out.println("Ticket pending confirmation: " + ticket.getId());
+            int rowsInserted = stmt.executeUpdate();
+            return rowsInserted > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * To confirm Ticket by User.
+     *
+     * @param ticketId ticket id
+     * @throws SQLException for connection
+     * @throws IOException  for generate bill
+     */
+    void commitTicket(int ticketId) throws SQLException, IOException {
+        connection.commit();
+        generateBill(ticketId);
+        System.out.println("Ticket confirmed and saved: " + ticketId);
+    }
+
+    /**
+     * To cancel Ticket by User.
+     *
+     * @param ticketId ticket id
+     * @throws SQLException for connection
+     */
+    void rollbackTicket(int ticketId) throws SQLException {
+        connection.rollback();
+        System.out.println("Transaction rolled back, ticket cancelled: " + ticketId);
+    }
+
+    /**
+     * To generate bill in text file
+     *
+     * @param ticketId Ticket id
+     * @throws SQLException for connection
+     * @throws IOException  for Buffered Writer
+     */
+    public void generateBill(int ticketId) throws SQLException, IOException {
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM ticket WHERE Id = ?");
+        stmt.setInt(1, ticketId);
+        ResultSet rs = stmt.executeQuery();
+        ResultSetMetaData rsmd = rs.getMetaData();
+
+        StringBuilder bill = new StringBuilder("=== TICKET BILL ===\n");
+        while (rs.next()) {
+            for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+                bill.append(rsmd.getColumnName(i)).append(": ").append(rs.getString(i)).append("\n");
+            }
+        }
+
+        String filename = "bill_" + ticketId + ".txt";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            writer.write(bill.toString());
+        }
+
+        System.out.println("Bill generated: " + filename);
+    }
+
+    /**
+     * To search Ticket by route and user.
+     *
+     * @param routeId route id
+     * @param userId  user id
+     * @throws SQLException for connection
+     */
+    public void searchTickets(int routeId, int userId) throws SQLException {
+        String sql = "SELECT * FROM ticket WHERE RouteId = ? OR UserId = ?";
+        PreparedStatement pstmt = connection.prepareStatement(sql);
+        pstmt.setInt(1, routeId);
+        pstmt.setInt(2, userId);
+
+        ResultSet rs = pstmt.executeQuery();
+        ResultSetMetaData rsmd = rs.getMetaData();
+
+        boolean found = false;
+        while (rs.next()) {
+            found = true;
+            System.out.println("---- TICKET FOUND ----");
+            for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+                System.out.println(rsmd.getColumnName(i) + ": " + rs.getString(i));
+            }
+        }
+
+        if (!found) {
+            System.out.println("No tickets found for route '" + routeId + "' or user '" + userId + "'.");
+        }
+    }
+}
---
+++ src/Model/Ticket.java
@@ -0,0 +1,110 @@
+package Model;
+
+import java.sql.Time;
+
+public class Ticket {
+    int id;
+    int userId;
+    int routeId;
+    boolean isBusTransport;
+    boolean isMetroTransport;
+    Time time;
+    double totalBill;
+    double distance;
+
+    /**
+     * Constructor for Ticket Class.
+     *
+     * @param id for ticket id
+     * @param userId for user id
+     * @param routeId for route id
+     * @param isBusTransport for bus transport
+     * @param isMetroTransport for metro transport
+     * @param time for current time
+     * @param totalBill for price per kilo meter
+     * @param distance for distance to travel
+     */
+    public Ticket(int id, int userId, int routeId, boolean isBusTransport, boolean isMetroTransport, Time time, double totalBill, double distance) {
+        this.id = id;
+        this.userId = userId;
+        this.routeId = routeId;
+        this.isBusTransport = isBusTransport;
+        this.isMetroTransport = isMetroTransport;
+        this.time = time;
+        this.totalBill = calculateBill();
+        this.distance = distance;
+    }
+
+    /**
+     * To calculate bill as per traveled distance.
+     *
+     * @return total amount
+     */
+    public double calculateBill(){
+        return distance * 5;
+    }
+
+    /**
+     * getter for bill id.
+     * @return Ticket's id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * getter for user id who is booking ticket.
+     * @return user id
+     */
+    public int getUserId() {
+        return userId;
+    }
+
+    /**
+     * getter for route in which user wants to travel.
+     * @return route id
+     */
+    public int getRouteId() {
+        return routeId;
+    }
+
+    /**
+     * getter user is traveling from Bus.
+     * @return true if it's Bus
+     */
+    public boolean isBusTransport() {
+        return isBusTransport;
+    }
+
+    /**
+     * getter for user traveling from Metro.
+     * @return true if it's Metro
+     */
+    public boolean isMetroTransport() {
+        return isMetroTransport;
+    }
+
+    /**
+     * getter for Booking time.
+     * @return time
+     */
+    public Time getTime() {
+        return time;
+    }
+
+    /**
+     * getter for user wants to travel distance.
+     * @return distance
+     */
+    public double getDistance() {
+        return distance;
+    }
+
+    /**
+     * getter for total bill.
+     * @return total amount to be paid by user
+     */
+    public double getTotalBill() {
+        return totalBill;
+    }
+}
