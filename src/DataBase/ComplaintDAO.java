6e6038da873e61dd572169c5c23e8f9b100ac501
PRANAY JAIN <jainpranaytaral@gmail.com>
2025-07-24 16:40:03 +0530

    init commit

--- src/DataBase/ComplaintDAO.java
+++ src/DataBase/ComplaintDAO.java
@@ -1,4 +1,57 @@
 package DataBase;
 
+import DataStructure.CustomQueue;
+import Model.Complaint;
+
+import java.sql.*;
+
 public class ComplaintDAO {
+    private CustomQueue<Complaint> complaintQueue = new CustomQueue<>();
+    private Connection connection = DataBaseManager.connection;
+
+    public ComplaintDAO() {
+        try {
+            DataBaseManager.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * To Register Complaint.
+     *
+     * @param c Object of Complaint
+     * @return true if complaint is filed
+     */
+    public boolean fileComplaint(Complaint c) {
+        String query = "INSERT INTO complaint (Department, UserId, Issue, Status) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            complaintQueue.enqueue(c);
+            stmt.setString(1, c.getDepartment());
+            stmt.setInt(2, c.getUserId());
+            stmt.setString(3, c.getIssue());
+            stmt.setBoolean(4, c.getStatus());
+            int rowsInserted = stmt.executeUpdate();
+            return rowsInserted > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * To solve complaint.
+     */
+    public void resolveNextComplaint() {
+        if (!complaintQueue.isEmpty()) {
+            Complaint c = complaintQueue.dequeue();
+            System.out.println("Resolving complaint:");
+            System.out.println("Department: " + c.getDepartment());
+            System.out.println("User: " + c.getUserId());
+            System.out.println("Issue: " + c.getIssue());
+            c.setStatus(true);
+        } else {
+            System.out.println("No complaints to resolve.");
+        }
+    }
 }
--- src/Model/Complaint.java
+++ src/Model/Complaint.java
@@ -1,4 +1,80 @@
 package Model;
 
 public class Complaint {
+    int id;
+    String department;
+    int userId;
+    String issue;
+    Boolean status;
+
+    /**
+     * Constructor for Complaint class.
+     *
+     * @param id         for complaint id
+     * @param department for which department complaint is registered
+     * @param userId     who is registered this complaint
+     * @param issue      what issue is in this complaint
+     * @param status     and what is status (false = Pending)
+     */
+    public Complaint(int id, String department, int userId, String issue, Boolean status) {
+        this.id = id;
+        this.department = department;
+        this.userId = userId;
+        this.issue = issue;
+        this.status = false;
+    }
+
+    /**
+     * getter for complaint id.
+     *
+     * @return complaint id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * getter for department on which complaint is registered.
+     *
+     * @return department id
+     */
+    public String getDepartment() {
+        return department;
+    }
+
+    /**
+     * getter for user who registered complaint.
+     *
+     * @return user id
+     */
+    public int getUserId() {
+        return userId;
+    }
+
+    /**
+     * getter for issue registered by user
+     *
+     * @return issue in department
+     */
+    public String getIssue() {
+        return issue;
+    }
+
+    /**
+     * getter for status (false = pending)
+     *
+     * @return status
+     */
+    public Boolean getStatus() {
+        return status;
+    }
+
+    /**
+     * setter ton set status.
+     *
+     * @param status if true issue is solved
+     */
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
 }
